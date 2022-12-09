c2490dda27f6c8a4145a039b6a57cbdff0eab8a4
CaelanTaylor <92650991+CaelanTaylor@users.noreply.github.com>
2022-12-09 19:16:19 +1300

    Update manjaro.sh

--- manjaro.sh
+++ manjaro.sh
@@ -1,1 +1,1 @@
-sudo pamac install discord kdenlive handbrake libreoffice-lib wine virtualbox virtualbox-ext-vnc virualbox-guest-iso blender vlc obs-studio
+sudo pamac install discord kdenlive handbrake libreoffice-fresh wine virtualbox virtualbox-ext-vnc virualbox-guest-iso blender vlc obs-studio
